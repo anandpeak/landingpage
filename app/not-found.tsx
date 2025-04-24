03de76892241c430bde8c014f7e0cb438580ab49
panda-desu <zr.freeze05@gmail.com>
2025-04-24 16:38:40 +0800

    added 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+// app/not-found.tsx
+
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center p-6">
+      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+      <p className="text-lg text-gray-500 mb-6">
+        Sorry, we couldn't find the page you're looking for.
+      </p>
+      <Link
+        className="cursor-pointer bg-[#ff8888] px-4 py-2 rounded-xl text-[#fff] "
+        href="/"
+      >
+        Go back home
+      </Link>
+    </div>
+  );
+}
