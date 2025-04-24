adfe1cdcc58a088f17b532067637aa45a8a67287
panda-desu <zr.freeze05@gmail.com>
2025-04-24 17:37:35 +0800

    added

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center min-h-screen text-center p-6">
       <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
       <p className="text-lg text-gray-500 mb-6">
-        Sorry, we couldn't find the page you're looking for.
+        Sorry, we couldn&apos;t find the page you&apos;re looking for.
       </p>
       <Link
         className="cursor-pointer bg-[#ff8888] px-4 py-2 rounded-xl text-[#fff] "
